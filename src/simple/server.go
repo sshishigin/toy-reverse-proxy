c71f429b67ebf2d9590019be66808aefbe040132
sshishig1n <sshishig1n@yandex.ru>
2022-08-27 15:23:37 +0300

    max_fails for servers

--- src/simple/server.go
+++ src/simple/server.go
@@ -9,13 +9,17 @@ type simpleServer struct {
 	Location  *url.URL
 	available bool
 	timeout   time.Duration
+	maxFails  int
+	fails     int
 }
 
 func (s *simpleServer) excludeWithTimeout() {
-	go func() {
+	if s.available {
 		s.available = false
+		log.Printf("excluding %s from bucket for %s", s.Location, s.timeout)
 		time.Sleep(s.timeout)
 		s.available = true
-	}()
-
+		s.fails = 0
+		log.Printf("%s is back", s.Location)
+	}
 }
